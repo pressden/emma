65814749e3a9d63849ceebaba0974db73d0123f3
D.S. Webster <dswebs.me@gmail.com>
2019-06-21 20:04:27 -0400

    Reduce whitespace to remain consistent with the rest of the theme

--- template-parts/site-info.php
+++ template-parts/site-info.php
@@ -1,11 +1,6 @@
 <div class="site-info">
-
   &copy;<?php echo date( 'Y' ); ?>
-
   <a href="<?php home_url(); ?>"><?php esc_html_e( get_bloginfo( 'name' ), 'emma' ); ?></a>
-
   <span class="sep"> | </span>
-
   <?php _e( 'All rights reserved.', 'emma' ); ?>
-
 </div><!-- .site-info -->
