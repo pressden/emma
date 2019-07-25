fb80609e14cd853cdf981b0402dc998e7b62f68c
D.S. Webster <dswebs.me@gmail.com>
2019-07-25 00:40:35 -0400

    Remove `.wrap` inside of `.entry-meta` since `.entry-header` now utilizes a `.wrap`

--- template-parts/entry-meta.php
+++ template-parts/entry-meta.php
@@ -1,14 +1,10 @@
 <?php if ( 'post' === get_post_type() ) : ?>
 
   <div class="entry-meta">
-    <div class="wrap">
-
-      <?php
-      emma_posted_on();
-      emma_posted_by();
-      ?>
-
-    </div><!-- .wrap -->
+    <?php
+    emma_posted_on();
+    emma_posted_by();
+    ?>
   </div><!-- .entry-meta -->
 
 <?php endif; ?>
