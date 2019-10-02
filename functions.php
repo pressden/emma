bd028fe9b1de782df5084c064f9c9268e033aa59
D.S. Webster <dswebs.me@gmail.com>
2019-10-02 09:46:29 -0400

    Temporarily remove the underscores Woocommerce overrides to examine Woocommerce defaults

--- functions.php
+++ functions.php
@@ -337,5 +337,5 @@ if ( defined( 'JETPACK__VERSION' ) ) {
  * Load WooCommerce compatibility file.
  */
 if ( class_exists( 'WooCommerce' ) ) {
-	require get_template_directory() . '/inc/woocommerce.php';
+	//require get_template_directory() . '/inc/woocommerce.php';
 }
