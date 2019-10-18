203bc0d1b84189d2b4ec4f039d5bdd7a6432d120
Eric Michel <eric@boultoncreative.com>
2019-10-18 12:15:58 -0400

    added a couple more theme support declarations and adjusted CSS for new template dynamic

--- functions.php
+++ functions.php
@@ -352,6 +352,9 @@ if ( class_exists( 'WooCommerce' ) ) {
  * Declare WooCommerce theme support
  */
 function declare_woocommerce_support() {
-	add_theme_support( 'woocommerce' );
+  add_theme_support( 'woocommerce' );
+  add_theme_support( 'wc-product-gallery-zoom' );
+  add_theme_support( 'wc-product-gallery-lightbox' );
+  add_theme_support( 'wc-product-gallery-slider' );
 }
 add_action( 'after_setup_theme', 'declare_woocommerce_support' );
