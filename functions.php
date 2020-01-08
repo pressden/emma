26419d68d7357f760a2f3dcbee6edce4eb8027fe
Eric Michel <eric@boultoncreative.com>
2020-01-08 16:34:57 -0500

    unformatted but functional pagination

--- functions.php
+++ functions.php
@@ -349,6 +349,11 @@ require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/menu-functions.php';
 
 /**
+ * Pagination.
+ */
+require get_template_directory() . '/inc/pagination.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
