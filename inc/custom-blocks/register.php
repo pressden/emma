4cc8ecbc107adca239670d93a58281fd222b88a9
D.S. Webster <dswebs.me@gmail.com>
2020-01-10 08:15:49 -0500

    Keep paren spacing consistent

--- inc/custom-blocks/register.php
+++ inc/custom-blocks/register.php
@@ -4,7 +4,7 @@ function load_responsive_grid_block() {
   wp_enqueue_script(
     'responsive-grid-block-js',
     get_template_directory_uri() . '/inc/custom-blocks/responsive-grid-block.js',
-    array('wp-blocks','wp-editor'),
+    array( 'wp-blocks','wp-editor' ),
     true
   );
   wp_enqueue_style(
@@ -15,4 +15,4 @@ function load_responsive_grid_block() {
 	);
 }
 
-add_action('enqueue_block_editor_assets', 'load_responsive_grid_block');
+add_action( 'enqueue_block_editor_assets', 'load_responsive_grid_block' );
