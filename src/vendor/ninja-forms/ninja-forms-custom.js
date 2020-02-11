a750b1acb8977cffa02e92d3b57ae978e6a4e2e4
Eric Michel <eric@boultoncreative.com>
2020-02-11 11:11:57 -0500

    changed analytics event firing method

--- src/vendor/ninja-forms/ninja-forms-custom.js
+++ src/vendor/ninja-forms/ninja-forms-custom.js
@@ -1,7 +1,10 @@
 jQuery( document ).ready( function() {
   jQuery( document ).on( 'nfFormSubmitResponse', function( event, response, id ) {
     if(window.ga && ga.create) {
-      ga( 'send', 'event', 'Contact', 'Form Submission', response.response.data.settings.title );
+      tracker = ga.getAll()[0];
+      if (tracker) {
+        tracker.send( 'event', 'Contact', 'Form Submission', response.response.data.settings.title );
+      }
     }
   });
 });
