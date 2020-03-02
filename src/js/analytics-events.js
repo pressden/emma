b3ee66d2db86765f60a0f506ecdd80a5ffd5956a
Eric Michel <eric@boultoncreative.com>
2020-03-02 12:34:07 -0500

    added ga events for phone call links, email links, and pdf download links

---
+++ src/js/analytics-events.js
@@ -0,0 +1,21 @@
+( function() {
+  document.addEventListener( 'click', function( event ) {
+    if(window.ga && ga.create) {
+      tracker = ga.getAll()[0];
+      if (tracker) {
+        var href = event.target.href;
+        if( href.substring( 0, 4 ) == 'tel:' ) {
+          tracker.send( 'event', 'Contact', 'Phone Call Link', href.substring( 4 ) );
+        }
+
+        if( href.substring( 0, 7 ) == 'mailto:' ) {
+          tracker.send( 'event', 'Contact', 'Email Link', href.substring( 7 ) );
+        }
+
+        if( href.substring( href.length - 4 ) == '.pdf' ) {
+          tracker.send( 'event', 'File', 'Download', href.substring( href.lastIndexOf('/') + 1 ) );
+        }
+      }
+    }
+  } );
+} ) ();
