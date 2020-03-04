1656e3d94a15d49e7935acd7de9e07598020d2e0
Eric Michel <eric@boultoncreative.com>
2020-03-04 09:42:40 -0500

    fixed js error on click because of analytics events

--- src/js/analytics-events.js
+++ src/js/analytics-events.js
@@ -4,16 +4,18 @@
       tracker = ga.getAll()[0];
       if (tracker) {
         var href = event.target.href;
-        if( href.substring( 0, 4 ) == 'tel:' ) {
-          tracker.send( 'event', 'Contact', 'Phone Number Click', href.substring( 4 ) );
-        }
+        if( href ) {
+          if( href.substring( 0, 4 ) == 'tel:' ) {
+            tracker.send( 'event', 'Contact', 'Phone Number Click', href.substring( 4 ) );
+          }
 
-        if( href.substring( 0, 7 ) == 'mailto:' ) {
-          tracker.send( 'event', 'Contact', 'Email Click', href.substring( 7 ) );
-        }
+          if( href.substring( 0, 7 ) == 'mailto:' ) {
+            tracker.send( 'event', 'Contact', 'Email Click', href.substring( 7 ) );
+          }
 
-        if( href.substring( href.length - 4 ) == '.pdf' ) {
-          tracker.send( 'event', 'File', 'Download', href.substring( href.lastIndexOf('/') + 1 ) );
+          if( href.substring( href.length - 4 ) == '.pdf' ) {
+            tracker.send( 'event', 'File', 'Download', href.substring( href.lastIndexOf('/') + 1 ) );
+          }
         }
       }
     }
