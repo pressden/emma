f7e8984f980edc91bb3401c7509e43ec2bd6c676
D.S. Webster <dswebs.me@gmail.com>
2020-04-23 00:40:29 -0400

    Change widget titles to h3 to lower their priority in the DOM

--- functions.php
+++ functions.php
@@ -256,8 +256,8 @@ function emma_widgets_init() {
       'description'   => esc_html__( 'Add widgets here.', 'emma' ),
       'before_widget' => '<section id="%1$s" class="widget %2$s">',
       'after_widget'  => '</section>',
-      'before_title'  => '<h2 class="widget-title">',
-      'after_title'   => '</h2>',
+      'before_title'  => '<h3 class="widget-title">',
+      'after_title'   => '</h3>',
     );
 
     $widget_area = wp_parse_args( $widget_area, $defaults );
