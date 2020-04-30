209d92d7566f67b1f556d02a15d77b4aeb850ba7
mmcachran <mattmcachran@gmail.com>
2020-04-30 14:25:39 -0400

    Spacing cleanup.

--- archive.php
+++ archive.php
@@ -14,7 +14,6 @@ get_header();
 		<main id="main" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
-
 			<header class="page-header">
 				<?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
@@ -26,17 +25,12 @@ get_header();
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-
 				get_template_part( 'template-parts/content', get_post_type() );
-
 			endwhile;
 
 			the_posts_navigation();
-
 		else :
-
 			get_template_part( 'template-parts/content', 'none' );
-
 		endif;
 		?>
 
