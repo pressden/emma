4fdc6f1b5db54ee48e176d05b9034c2bf1738ddb
Eric Michel <eric@boultoncreative.com>
2020-06-08 16:15:32 -0400

    removed custom text size selector from Heading block - it is now a native feature

--- src/js/emma-admin.js
+++ src/js/emma-admin.js
@@ -1,6 +1,5 @@
 /*--------------------------------------------------------------
 # Emma - Admin Scripts
 --------------------------------------------------------------*/
-import "./editor/header-block.js";
 import "./editor/responsive-grid-block.js";
 import "./editor/slider-block.js";
