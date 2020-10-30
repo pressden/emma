8e2258cab94217b92719c6ad1223abc762518994
Eric Michel <eric@boultoncreative.com>
2020-10-30 17:21:14 -0400

    got srcset working

--- src/js/lightgallery.js
+++ src/js/lightgallery.js
@@ -1,7 +1,15 @@
 ( function() {
   document.querySelectorAll( ".lightgallery" ).forEach( gallery => {
+    gallery.querySelectorAll( 'figure a' ).forEach( anchor => {
+      var img = anchor.querySelector( 'img' );
+      var srcset = img.srcset;
+      var sizes = img.sizes;
+      anchor.dataset.srcset = srcset;
+      anchor.dataset.sizes = sizes;
+    } );
     lightGallery( gallery, {
-      selector: 'figure a'
+      selector: 'figure a',
+      download: false,
     } );
   } );
 } )();
