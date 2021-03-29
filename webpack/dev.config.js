e5cbbd95c06f94e96e8723aa17ea410596394a6b
D.S. Webster <dswebsme@users.noreply.github.com>
2021-03-28 22:32:40 -0400

    Update, prettify and bump the version (#153)

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,6 +1,6 @@
-const baseConfig = require('./base.config.js');
-const { merge } = require('webpack-merge');
+const baseConfig = require("./base.config.js");
+const { merge } = require("webpack-merge");
 
 module.exports = merge(baseConfig, {
-  devtool: 'eval-source-map',
+	devtool: "eval-source-map",
 });
