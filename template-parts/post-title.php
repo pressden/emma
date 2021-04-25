a8d6788d25f90f167a92a35f9c63b0d6d0ef5349
D.S. Webster <dswebs.me@gmail.com>
2021-04-24 21:43:51 -0400

    Clean up post template parts

--- template-parts/post-title.php
+++ template-parts/post-title.php
@@ -1,6 +1,6 @@
 <?php
-if ( is_singular() || $post->ID == get_option('page_for_posts') ) :
-  the_title( '<h1 class="entry-title">', '</h1>' );
+if ( is_singular() || get_option( 'page_for_posts' ) === $post->ID ) :
+	the_title( '<h1 class="entry-title">', '</h1>' );
 else :
-  the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+	the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 endif;
