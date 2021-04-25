c2616ee9fd5bb52edb8e793ca6ac020d8d787c20
D.S. Webster <dswebs.me@gmail.com>
2021-04-24 21:56:44 -0400

    Clean up sitewide template parts

--- template-parts/site-branding.php
+++ template-parts/site-branding.php
@@ -1,24 +1,26 @@
 <div class="site-branding">
 
-  <?php
-  /**
-   * Fires inside the site-branding markup.
-   *
-   * @since 1.0.0
-   */
-  do_action( 'emma_site_branding' );
-  ?>
+	<?php
+	/**
+	 * Fires inside the site-branding markup.
+	 *
+	 * @since 1.0.0
+	 */
+	do_action( 'emma_site_branding' );
+	?>
 
-  <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+	<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 
-  <?php
-  $emma_description = get_bloginfo( 'description', 'display' );
+	<?php
+	$emma_description = get_bloginfo( 'description', 'display' );
 
-  if ( $emma_description || is_customize_preview() ) :
-    ?>
+	if ( $emma_description || is_customize_preview() ) {
+		?>
 
-    <p class="site-description"><?php echo $emma_description; ?></p>
+		<p class="site-description"><?php echo $emma_description; ?></p>
 
-  <?php endif; ?>
+		<?php
+	}
+	?>
 
 </div><!-- .site-branding -->
