86af7fcac37cfd4f28c2d77741a62e89398c8209
Eric Michel <eric.r.michel@gmail.com>
2021-04-26 13:39:15 -0400

    created testing ground for getting custom fields working for feature links

--- inc/menu-functions.php
+++ inc/menu-functions.php
@@ -37,24 +37,34 @@ function emma_feature_link_meta_box_content() {
 	 */
 	class Feature {};
 
+	$feature             = new Feature();
+	$feature->classes    = array();
+	$feature->type       = 'custom';
+	$feature->title      = 'Search';
+	$feature->url        = '#';
+	$feature->target     = 'emma-feature-link_search-form-toggle';
+	$feature->object_id  = 'gets-overwritten';
+	$feature->object     = 'gets-overwritten';
+	$feature->type_label = 'gets-overwritten';
+
+	$features[] = $feature;
+
 	$feature            = new Feature();
-	$feature->classes   = array( 'search-form-toggle' );
+	$feature->classes   = array();
 	$feature->type      = 'custom';
-	$feature->object_id = 'search-toggle';
-	$feature->title     = 'Search';
-	$feature->object    = 'custom';
+	$feature->title     = 'Login';
 	$feature->url       = '#';
+	$feature->object_id  = '-1';
 
 	$features[] = $feature;
 
 	if ( class_exists( 'WooCommerce' ) ) {
 		$feature            = new Feature();
-		$feature->classes   = array( 'mini-cart-toggle' );
+		$feature->classes   = array();
 		$feature->type      = 'custom';
-		$feature->object_id = 'mini-cart-toggle';
 		$feature->title     = 'Cart';
-		$feature->object    = 'custom';
 		$feature->url       = get_permalink( wc_get_page_id( 'cart' ) );
+		$feature->object_id  = '-1';
 
 		$features[] = $feature;
 	}
@@ -110,3 +120,56 @@ function emma_change_menu_hidden_metaboxes( $user_id ) {
 	update_user_option( $user_id, 'metaboxhidden_nav-menus', $hidden_metaboxes );
 }
 add_action( 'user_register', 'emma_change_menu_hidden_metaboxes', 10, 1 );
+
+
+function emma_feature_link_custom_fields( $item_id, $item ) {
+	$feature_link_meta = get_post_meta( $item_id, '_feature_link', true );
+	if( substr( $item->target, 0, 17 ) === 'emma-feature-link' || $feature_link_meta ) {
+	
+		$feature_link_class = $feature_link_meta ? $feature_link_meta : substr( $item->target, strpos( $item->target, "_" ) + 1 );
+
+		wp_nonce_field( 'feature_link_nonce', '_feature_link_nonce_name' );
+		?>
+		<div class="field-custom_menu_meta description-wide" style="margin: 5px 0;">
+				<span class="description">Here's a helpful note to describe what this Feature Link does.</span>
+				<input type="hidden" class="nav-menu-id" value="<?php echo $item_id ;?>" />
+				<div class="logged-input-holder">
+					<input type="text" name="feature_link[<?php echo $item_id; ?>]" id="feature-link-for-<?php echo $item_id ;?>" value="<?php echo $feature_link_class; ?>"; />
+				</div>
+		</div>
+
+		<div style="clear:both"><?php var_dump( $item ); ?></div>
+		<?php
+	}
+}
+add_action( 'wp_nav_menu_item_custom_fields', 'emma_feature_link_custom_fields', 10, 2 );
+
+function emma_feature_link_custom_fields_save( $menu_id, $menu_item_db_id ) {
+
+	// Verify this came from our screen and with proper authorization.
+	if ( ! isset( $_POST['_feature_link_nonce_name'] ) || ! wp_verify_nonce( $_POST['_feature_link_nonce_name'], 'feature_link_nonce' ) ) {
+    return $menu_id;
+	}
+
+	if ( isset( $_POST['feature_link'][$menu_item_db_id] ) ) {
+		$sanitized_data = sanitize_text_field( $_POST['feature_link'][$menu_item_db_id] );
+		update_post_meta( $menu_item_db_id, '_feature_link', $sanitized_data );
+	} else {
+		delete_post_meta( $menu_item_db_id, '_feature_link' );
+	}
+}
+add_action( 'wp_update_nav_menu_item', 'emma_feature_link_custom_fields_save', 10, 2 );
+
+function emma_feature_link_class_output( $classes, $item ) {
+
+	if( is_object( $item ) && isset( $item->ID ) ) {
+
+		$feature_link_class = get_post_meta( $item->ID, '_feature_link', true );
+
+		if ( ! empty( $feature_link_class ) ) {
+			$classes[] = $feature_link_class;
+		}
+	}
+	return $classes;
+}
+add_filter( 'nav_menu_css_class', 'emma_feature_link_class_output', 10, 2 );
