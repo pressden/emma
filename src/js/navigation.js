ee379976f8c9081d9d05fd6cf1147d08e33618e7
D.S. Webster <dswebs.me@gmail.com>
2021-06-28 08:06:19 -0400

    Clean up remaining JS

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -7,120 +7,127 @@
 
 var toggle, closer, drawer, clones;
 
-( function() {
-  var collection, container, menu, links, i, len;
+(function () {
+	var collection, container, menu, links, i, len;
 
-  toggle = document.getElementById( 'menu-opener' );
-  closer = document.getElementById( 'menu-closer' );
-  drawer = document.getElementById( 'menu-drawer' );
-  clones = document.getElementById( 'menu-clones' );
+	toggle = document.getElementById("menu-opener");
+	closer = document.getElementById("menu-closer");
+	drawer = document.getElementById("menu-drawer");
+	clones = document.getElementById("menu-clones");
 
-  collection = document.querySelectorAll( '#masthead .site-navigation, #masthead .widget_nav_menu, #main-navigation' );
+	collection = document.querySelectorAll(
+		"#masthead .site-navigation, #masthead .widget_nav_menu, #main-navigation"
+	);
 
-  // Exit early if collection is empty or the toggle button is undefined
-	if ( collection.length === 0 || 'undefined' === typeof toggle ) {
+	// Exit early if collection is empty or the toggle button is undefined
+	if (collection.length === 0 || "undefined" === typeof toggle) {
 		return;
-  }
+	}
 
-  toggle.onclick = function() {
-    if ( ! drawer.classList.contains( 'toggled' ) ) {
-      openMenuDrawer();
-    } else {
-      closeMenuDrawer();
-    }
-  };
+	toggle.onclick = function () {
+		if (!drawer.classList.contains("toggled")) {
+			openMenuDrawer();
+		} else {
+			closeMenuDrawer();
+		}
+	};
 
-  closer.onclick = function() {
-    closeMenuDrawer();
-  };
+	closer.onclick = function () {
+		closeMenuDrawer();
+	};
 
-  closer.addEventListener( 'blur', function( event ) {
-    if( ! drawer.contains( event.relatedTarget ) ) {
-      closeMenuDrawer();
-    }
-  } );
+	closer.addEventListener("blur", function (event) {
+		if (!drawer.contains(event.relatedTarget)) {
+			closeMenuDrawer();
+		}
+	});
 
-  collection.forEach( container => {
-    menu = container.querySelector( 'ul' );
+	collection.forEach((container) => {
+		menu = container.querySelector("ul");
 
-    // put a clone of the menu in the menu clones container
-    let clone = menu.cloneNode( true );
-    clones.appendChild( clone );
+		// put a clone of the menu in the menu clones container
+		let clone = menu.cloneNode(true);
+		clones.appendChild(clone);
 
-    if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
-      menu.className += ' nav-menu';
-    }
-  } );
+		if (-1 === menu.className.indexOf("nav-menu")) {
+			menu.className += " nav-menu";
+		}
+	});
 
-  clones.querySelectorAll( '*' ).forEach( item => {
-    if( item.id ) {
-      item.id = item.id + "-drawer";
-    }
-  } );
+	clones.querySelectorAll("*").forEach((item) => {
+		if (item.id) {
+			item.id = item.id + "-drawer";
+		}
+	});
 
-  document.querySelectorAll( '.menu-item-has-children > a' ).forEach( item => {
-    item.addEventListener( 'touchend', function( event ) {
-      if( item !== document.activeElement ) {
-        item.focus();
-        event.preventDefault();
-      }
-    } );
-  } );
+	document.querySelectorAll(".menu-item-has-children > a").forEach((item) => {
+		item.addEventListener("touchend", function (event) {
+			if (item !== document.activeElement) {
+				item.focus();
+				event.preventDefault();
+			}
+		});
+	});
 
-  // Get all the link elements within the menu.
-  drawer.querySelectorAll( '.menu-item-has-children > a' ).forEach( item => {
-    item.addEventListener( 'focus', function( event ) {
-      toggleFocus( this );
-    } );
-    //stops focus from being given (and thus menu from appearing) before click event is registered
-    item.addEventListener( 'mousedown', function( event ) {
-      event.preventDefault();
-    } );
-    item.addEventListener( 'click', function( event) {
-      if( ! this.closest( '.menu-item-has-children' ).classList.contains( 'focus' ) ) {
-        event.preventDefault();
-        this.focus();
-      }
-    } );
-  } );
+	// Get all the link elements within the menu.
+	drawer.querySelectorAll(".menu-item-has-children > a").forEach((item) => {
+		item.addEventListener("focus", function (event) {
+			toggleFocus(this);
+		});
+		//stops focus from being given (and thus menu from appearing) before click event is registered
+		item.addEventListener("mousedown", function (event) {
+			event.preventDefault();
+		});
+		item.addEventListener("click", function (event) {
+			if (
+				!this.closest(".menu-item-has-children").classList.contains("focus")
+			) {
+				event.preventDefault();
+				this.focus();
+			}
+		});
+	});
 
-  drawer.addEventListener( 'focusout', function( event ) {
-    var target = event.target;
-    var menuParent = target.closest( '.menu-item-has-children' );
-    if( menuParent ) {
-      if ( ! menuParent.contains( event.relatedTarget ) ) {
-        menuParent.classList.remove( 'focus' );
-      }
-    }
+	drawer.addEventListener("focusout", function (event) {
+		var target = event.target;
+		var menuParent = target.closest(".menu-item-has-children");
+		if (menuParent) {
+			if (!menuParent.contains(event.relatedTarget)) {
+				menuParent.classList.remove("focus");
+			}
+		}
 
-    if( ! drawer.contains( event.relatedTarget ) ) {
-      closer.focus();
-    }
-  } );
-} )();
+		if (!drawer.contains(event.relatedTarget)) {
+			closer.focus();
+		}
+	});
+})();
 
 function openMenuDrawer() {
-  drawer.querySelectorAll( 'a' ).forEach( item => {
-    item.tabIndex = 0;
-  } );
-  drawer.classList.add( 'toggled' );
-  closer.focus();
+	drawer.querySelectorAll("a").forEach((item) => {
+		item.tabIndex = 0;
+	});
+	drawer.classList.add("toggled");
+	closer.focus();
 }
 
 function closeMenuDrawer() {
-  drawer.querySelectorAll( 'a' ).forEach( item => {
-    item.tabIndex = -1;
-  } );
-  drawer.classList.remove( 'toggled' );
-  toggle.focus();
+	drawer.querySelectorAll("a").forEach((item) => {
+		item.tabIndex = -1;
+	});
+	drawer.classList.remove("toggled");
+	toggle.focus();
 }
 
 /**
  * Sets or removes .focus class on an element.
  */
-function toggleFocus( menuItem ) {
-  drawer.querySelectorAll( '.focus' ).forEach( focusItem => function() {
-    focusItem.classList.remove( 'focus' );
-  } );
-  menuItem.closest( 'li' ).classList.add( 'focus' );
+function toggleFocus(menuItem) {
+	drawer.querySelectorAll(".focus").forEach(
+		(focusItem) =>
+			function () {
+				focusItem.classList.remove("focus");
+			}
+	);
+	menuItem.closest("li").classList.add("focus");
 }
--- src/js/utility.js
+++ src/js/utility.js
@@ -1,20 +1,20 @@
-( function() {
-  document.addEventListener( 'click', function( event ) {
-    var link_extend = event.target.closest( '.link-extend' )
-    if( link_extend ) {
-      var anchor = link_extend.querySelector( 'a' );
-      if( anchor ) {
-        var href = anchor.getAttribute( 'href' );
-        var target = anchor.getAttribute( 'target' );
+(function () {
+	document.addEventListener("click", function (event) {
+		var link_extend = event.target.closest(".link-extend");
+		if (link_extend) {
+			var anchor = link_extend.querySelector("a");
+			if (anchor) {
+				var href = anchor.getAttribute("href");
+				var target = anchor.getAttribute("target");
 
-        if( href ) {
-          if( target ) {
-            window.open( href, target );
-          } else {
-            window.location = anchor.getAttribute( 'href' );
-          }
-        }
-      }
-    }
-  } );
-} )();;
+				if (href) {
+					if (target) {
+						window.open(href, target);
+					} else {
+						window.location = anchor.getAttribute("href");
+					}
+				}
+			}
+		}
+	});
+})();
