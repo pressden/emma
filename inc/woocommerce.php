457ac77cefdd962fce2c3246e54a8d4cddc98b21
D.S. Webster <dswebs.me@gmail.com>
2021-06-27 20:08:01 -0400

    Remove redundant code. Woocommerce matches our expected wrappers 1-to-1.

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -88,50 +88,6 @@ if ( ! function_exists( 'emma_woocommerce_product_columns_wrapper_close' ) ) {
 add_action( 'woocommerce_after_shop_loop', 'emma_woocommerce_product_columns_wrapper_close', 40 );
 
 /**
- * Remove default WooCommerce wrapper.
- */
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
-
-if ( ! function_exists( 'emma_woocommerce_wrapper_before' ) ) {
-	/**
-	 * Before Content.
-	 *
-	 * Wraps all WooCommerce content in wrappers which match the theme markup.
-	 *
-	 * @return void
-	 */
-	function emma_woocommerce_wrapper_before() {
-		?>
-
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
-
-			<?php
-	}
-}
-add_action( 'woocommerce_before_main_content', 'emma_woocommerce_wrapper_before' );
-
-if ( ! function_exists( 'emma_woocommerce_wrapper_after' ) ) {
-	/**
-	 * After Content.
-	 *
-	 * Closes the wrapping divs.
-	 *
-	 * @return void
-	 */
-	function emma_woocommerce_wrapper_after() {
-		?>
-
-			</main><!-- #main -->
-		</div><!-- #primary -->
-
-		<?php
-	}
-}
-add_action( 'woocommerce_after_main_content', 'emma_woocommerce_wrapper_after' );
-
-/**
  * Sample implementation of the WooCommerce Mini Cart.
  *
  * You can add the WooCommerce Mini Cart to header.php like so ...
