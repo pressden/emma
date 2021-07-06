1798479f11d8c025d97761efb106bac5433b23e3
D.S. Webster <dswebs.me@gmail.com>
2021-07-06 07:32:04 -0400

    Improve the default styling of various elements

--- template-parts/site-info.php
+++ template-parts/site-info.php
@@ -1,6 +1,6 @@
 <div class="site-info">
 	&copy;<?php echo date( 'Y' ); ?>
-	<a href="<?php echo home_url(); ?>"><?php esc_html( get_bloginfo( 'name' ) ); ?></a>
+	<a href="<?php echo home_url(); ?>"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
 	<span class="sep"> | </span>
 	<?php esc_html_e( 'All rights reserved.', 'emma' ); ?>
 </div><!-- .site-info -->
