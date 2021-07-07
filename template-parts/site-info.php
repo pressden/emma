bed630f4574fd59127ee0344894ed0c626d7718f
D.S. Webster <dswebs.me@gmail.com>
2021-07-07 09:48:45 -0400

    Improve default copyright statement

--- template-parts/site-info.php
+++ template-parts/site-info.php
@@ -1,5 +1,5 @@
 <div class="site-info">
-	&copy;<?php echo date( 'Y' ); ?>
+	Copyright &copy; <?php echo date( 'Y' ); ?>
 	<a href="<?php echo home_url(); ?>"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
 	<span class="sep"> | </span>
 	<?php esc_html_e( 'All rights reserved.', 'emma' ); ?>
