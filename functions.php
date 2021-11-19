47f798504d6acdf9de4479bcbe0a8182e31c1b43
Eric Michel <eric.r.michel@gmail.com>
2021-11-19 15:57:08 -0500

    removed old flyout menu widgets registration

--- functions.php
+++ functions.php
@@ -318,18 +318,6 @@ function emma_widgets_init() {
 		);
 	}
 
-	// Filter 'emma_flyout_menu_widget_areas' in the global scope for use in templates.
-	$GLOBALS['emma_flyout_menu_widget_areas'] = apply_filters( 'emma_flyout_menu_widget_areas', 2 );
-
-	// Add the flyout menu widget areas.
-	for ( $i = 1; $i <= $GLOBALS['emma_flyout_menu_widget_areas']; $i++ ) {
-		$widget_areas[ 'flyout-menu-widgets-' . $i ] = array(
-			'name'        => esc_html__( 'Flyout Menu Widgets', 'emma' ) . "  $i",
-			'id'          => 'flyout-menu-widgets-' . $i,
-			'description' => esc_html__( 'Add widgets to the slide-out menu', 'emma' ) . ' (' . esc_html__( 'column', 'emma' ) . " $i).",
-		);
-	}
-
 	// Filter 'emma_footer_widget_areas' in the global scope for use in templates.
 	$GLOBALS['emma_footer_widget_areas'] = apply_filters( 'emma_footer_widget_areas', 1 );
 
