a85d051b947007a07feaab277816fd501c9c853a
D.S. Webster <dswebs.me@gmail.com>
2021-12-12 15:27:34 -0500

    Add before and after hooks to the primary menu items

--- template-parts/navigation-primary.php
+++ template-parts/navigation-primary.php
@@ -2,19 +2,40 @@
 
 	<nav id="main-navigation" class="site-navigation main-navigation">
 		<div class="wrap">
+			<div class="menu-container">
+				<ul id="primary-menu" class="menu">
 
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location'  => 'primary',
-					'menu_id'         => 'primary-menu',
-					'container_class' => 'menu-container',
-				)
-			);
-			?>
+					<?php
+					/**
+					 * Fires before the primary menu items.
+					 *
+					 * @since 1.0.0
+					 */
+					do_action( 'emma_before_primary_menu_items' );
+					?>
 
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'primary',
+							'container'      => 'false',
+							'items_wrap'     => '%3$s',
+						)
+					);
+					?>
+
+					<?php
+					/**
+					 * Fires after the primary menu items.
+					 *
+					 * @since 1.0.0
+					 */
+					do_action( 'emma_after_primary_menu_items' );
+					?>
+
+				</ul><!-- #primary-menu -->
+			</div><!-- .menu-container -->
 		</div><!-- .wrap -->
-
 	</nav><!-- #main-navigation -->
 
 <?php endif; ?>
