72ae0ca2ddcbfd9fead42b6cadf7b7a4f6347419
D.S. Webster <dswebs.me@gmail.com>
2022-03-12 14:59:55 -0500

    Improve targeting of .site-info contents

--- template-parts/site-info.php
+++ template-parts/site-info.php
@@ -1,6 +1,15 @@
 <div class="site-info">
-	Copyright &copy; <?php echo date( 'Y' ); ?>
-	<a href="<?php echo esc_url( home_url() ); ?>"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
+
+	<span class="copyright-label">Copyright</span>
+
+	<span class="copyright-symbol">&copy; <?php echo wp_kses_post( date( 'Y' ) ); ?></span>
+
+	<a href="<?php echo esc_url( home_url() ); ?>" class="site-name">
+		<?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+	</a>
+
 	<span class="sep"> | </span>
-	<?php esc_html_e( 'All rights reserved.', 'emma' ); ?>
+
+	<span class="copyright-rights"><?php esc_html_e( 'All rights reserved.', 'emma' ); ?></span>
+
 </div><!-- .site-info -->
