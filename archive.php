e1386a0e3efd97c037a937ad086c00b0242cba85
Eric Michel <eric.r.michel@gmail.com>
2022-09-27 16:29:05 -0400

    mostly full rework of templates into hybrid block theme

--- archive.php
+++ archive.php
@@ -12,44 +12,11 @@ get_header();
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="entry-header">
-				<div class="wrap">
-
-					<?php
-					the_archive_title( '<h1 class="entry-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-					?>
-
-				</div><!-- .wrap -->
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) :
-					the_post();
-					get_template_part( 'template-parts/content', get_post_type() );
-				endwhile;
-
-				emma_pagination();
-				?>
-
-			</div><!-- .page-content -->
-
-			<?php
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-		endif;
-		?>
-
+		<div class="page-content wp-site-blocks">
+			<?php block_template_part( 'archive-content' ); ?>
+		</div><!-- .page-content -->
 	</main><!-- #main -->
 </div><!-- #primary -->
 
 <?php
-get_template_part( 'template-parts/sidebars' );
-
 get_footer();
--- index.php
+++ index.php
@@ -15,41 +15,13 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			emma_pagination();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
+		<div class="page-content wp-site-blocks">
+			<?php block_template_part( 'index-content' ); ?>
+		</div><!-- .page-content -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
-get_template_part( 'template-parts/sidebars' );
-
 get_footer();
