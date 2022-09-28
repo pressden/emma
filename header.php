ca3f92ac16a041ab5fc4dd97d9f3351ece5d804a
D.S. Webster <dswebs.me@gmail.com>
2022-09-28 17:55:49 -0400

    Simplyfy viewport meta definition and make it hookable

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<meta name="viewport" content="<?php echo apply_filters( 'emma_viewport_meta', 'width=device-width, initial-scale=1' ); ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
